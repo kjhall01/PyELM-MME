9adca098314292d1dcb102b5d72543d6f6f44498
kjhall01 <kjhall01@email.wm.edu>
2021-03-23 17:20:00 -0400

    so close to done packaging

--- bin/pyelm2d.py
+++
@@ -1,9 +0,0 @@
-from subprocess import Popen, PIPE
-from pathlib import Path
-import pyelmmme as _
-
-
-if __name__=='__main__':
-	wd = Path(_.__path__)
-	proc = Popen(['jupyter', 'notebook', str( (wd / 'bin' / PyELM-MME-2D.ipynb').absolute()) ] stdout=PIPE, stderr=PIPE) 
- 
